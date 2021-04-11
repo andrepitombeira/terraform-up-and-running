23136eedf9f42c6d2afa17aa39ad6aafc6ad7e7c
Andre Pitombeira <andrepitombeira@gmail.com>
2021-04-11 11:05:34 +0200

    Use S3 to store terraform state

---
+++ state/backend.hcl
@@ -0,0 +1,4 @@
+bucket = "terraform-up-and-running-state-bucket-12345"
+region = "eu-central-1"
+dynamodb_table = "terraform-up-and-running-locks"
+encrypt = true
---
+++ state/state.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-bucket-12345"
+
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  # Enable versioning so we can see the full revision history of our static file
+  versioning {
+    enabled = true
+  }
+
+  # Enale server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
+
+terraform {
+  backend "s3" {
+    key = "global/s3/terraform.tfstate"
+  }
+}
